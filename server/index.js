648997ff3e0d35c4ca2489b8fda4ffe51857f9bb
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-07-13 15:03:00 +0530

    done

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'https://arabiaseel.com',
-  'https://gvs-projects.vercel.app'
+  'https://arabiaseel.vercel.app'
 ];
 
 app.use(cors({
