20454a9842a9350b57947880dbe0bd213d0c56c6
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-07-29 23:50:43 +0530

    done

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 // --- ROBUST CORS Configuration for Production ---
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://arabiaseel.com',
-  'https://arabiaseel.vercel.app'
+  'https://arabiaseel.com/',
+  'https://arabiaseel.vercel.app/'
 ];
 
 const corsOptions = {
