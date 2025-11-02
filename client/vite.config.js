edcc94b56ccc652f2b1239c7614abe9dc5c4bc89
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-11-02 13:57:28 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://arabi-aseel-1.onrender.com', 
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://arabi-aseel-1.onrender.com', 
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
