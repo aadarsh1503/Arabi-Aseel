07f339095ee30a97afd861cb95079d39cefa0609
aadarsh1503 <aadarshchauhan35@gmail.com>
2026-02-15 15:49:44 +0530

    done

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create a single, intelligent Axios instance.
 // REMOVED the default 'Content-Type' header. Axios will handle it automatically.
 const api = axios.create({
-  baseURL:"/api",
+  baseURL:"https://arabi-aseel-1.onrender.com/api",
 });
 
 // --- Smart Request Interceptor ---
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', 
+        target: 'https://arabi-aseel-1.onrender.com', 
         changeOrigin: true,
         secure: false,
       },
